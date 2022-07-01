c98aa2025f5c0b598fc7fe169a059b6ae128b63d
wernnon <ruizwernnon@gmail.com>
2022-07-01 13:08:28 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+//App.jsx
+import './App.css'
+import List from './List.jsx'
+
+function App(){
+
+return(
+ //bloco   
+    <div>
+
+            <p className='text'>
+                Mais lidas
+            </p>  
+            <List />
+
+        <div className='text'>
+       
+            Mais lidas
+        
+        </div>
+        
+        <List/>  
+
+
+    </div>
+)
+}
+export default App;
